b31140fe57e0c993d1eae3ebac62a291a16993c5
Del Reid <45520351+itsjustdel@users.noreply.github.com>
2022-07-21 10:30:12 +0100

    Default Layouts

--- Assets/Scripts/Layout.cs
+++ Assets/Scripts/Layout.cs
@@ -98,13 +98,99 @@ public class Layout
 
     public static float scaleOverall = .6f;
 
-    public static void DefaultLayoutsWIP(GameObject dialsPrefab)
+    public static void DefaultLayouts(GameObject dialsPrefab)
     {
-        //find max scale to use for each dial
-
         //group any 2 engine dials, they need to be beside each other
         List<GameObject> activeDials = ActiveDials(dialsPrefab);
-        Dictionary<string, List<GameObject>> dials = new Dictionary<string, List<GameObject>>();
+
+        //find max scale to use for each dial
+        RectTransform rectTransform = GameObject.FindGameObjectWithTag("Canvas").GetComponent<RectTransform>();
+
+        int columns = 8;
+        int rows = 2;
+        
+        if (activeDials.Count > 6)
+        {
+            columns = 5;
+            rows = 3;        
+        }
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+        
+        Canvas canvas = GameObject.FindGameObjectWithTag("Canvas").GetComponent<Canvas>();
+        
+        //step is what's smaller from the height and width ratios
+        float step = Mathf.Min( parentWidth/ columns, parentHeight / rows);
+        //face - width 100 scale 8 so width = 800
+        float scale = step/800;        
+        float start = -parentWidth * .5f + step * .5f;
+        float x = start;
+        float y = parentHeight*.5f - step*.5f;
+
+        int thisRowCount = 0;
+
+        
+        float lastDiff = 0f;
+        for (int i = 0; i < activeDials.Count; i++)
+        {
+            bool newRow = false;
+            if (i + 1 < activeDials.Count
+                    && activeDials[i].tag != "Untagged" 
+                        && activeDials[i].tag == activeDials[i + 1].tag
+                            //next dial's position over screen edge?
+                            && x + step> parentWidth*.5f)
+                newRow = true;
+
+            if (newRow)
+            {
+                Debug.Log("new row at " + activeDials[i].name);
+                x = start;
+                y -= step;
+
+                float diffNewRow = (parentWidth * .5f) / thisRowCount;
+                //move all dials in this row along to center
+                for (int j = 0; j < thisRowCount; j++)
+                {
+                    activeDials[i - 1 - j].GetComponent<RectTransform>().anchoredPosition += new Vector2(diffNewRow, 0);
+                }
+
+                thisRowCount = 0;                
+            }
+
+            activeDials[i].GetComponent<RectTransform>().anchoredPosition = new Vector2(x, y);
+
+            x += step;
+            thisRowCount++;
+            
+            //will next dial be over the edge of the screen?
+            if(x > parentWidth*.5f)
+            {
+                x = start;
+                y -= step;
+                float diff = parentWidth * .5f - x;
+                //move all dials in this row along to center
+                for (int j = 0; j < thisRowCount; j++)
+                {
+                    activeDials[i-j].GetComponent<RectTransform>().anchoredPosition -= new Vector2(diff, 0);
+                }
+
+                thisRowCount = 0;
+                lastDiff = diff;
+            }
+
+            activeDials[i].transform.localScale = new Vector3(scale, scale, scale);
+        }
+
+        //move all dials in this row along to center - note diff polarity swap
+        for (int j = 0; j < thisRowCount; j++)
+        {
+            activeDials[activeDials.Count-1 - j].GetComponent<RectTransform>().anchoredPosition -= new Vector2(lastDiff-step*.5f, 0);
+        }
+    }
+
+    /*
+     * Dictionary<string, List<GameObject>> dials = new Dictionary<string, List<GameObject>>();
         for (int i = 0; i < activeDials.Count; i++)
         {
             //create a new key with a new list if key doesn't exist
@@ -118,32 +204,40 @@ public class Layout
 
         }
 
-        foreach (var kvp in dials)
-        {         
-            Debug.Log(kvp.Key + kvp.Value);
-            List<GameObject> gs = kvp.Value;
-            foreach (var g in gs)
-            {
-                Debug.Log(g.name);
+     */
 
+    /// Calculates the optimal side of squares to be fit into a rectangle
+    /// Inputs: x, y: width and height of the available area.
+    ///         n: number of squares to fit
+    /// Returns: the optimal side of the fitted squares
+    private static float FitSquares(float x, float y, int n)
+    {
+        float sx, sy;
 
-            }
+        var px = Mathf.Ceil(Mathf.Sqrt(n * x / y));
+        if (Mathf.Floor(px * y / x) * px < n)
+        {
+            sx = y / Mathf.Ceil(px * y / x);
+        }
+        else
+        {
+            sx = x / px;
         }
 
-        //find orientation/screen size
-        GameObject canvasObject = GameObject.FindGameObjectWithTag("Canvas");
-        float canvasX = canvasObject.GetComponent<RectTransform>().rect.width;
-        float canvasY = canvasObject.GetComponent<RectTransform>().rect.height;
+        var py = Mathf.Ceil(Mathf.Sqrt(n * y / x));
+        if (Mathf.Floor(py * x / y) * py < n)
+        {
+            sy = x / Mathf.Ceil(x * py / y);
+        }
+        else
+        {
+            sy = y / py;
+        }
 
-        Debug.Log(Screen.orientation);
-
-        Debug.Log("x = " + canvasX);
-        Debug.Log("y = " + canvasY);
-
-
+        return Mathf.Max(sx, sy);
     }
 
-    public static void DefaultLayouts(GameObject dialsPrefab)
+    public static void DefaultLayoutsOld(GameObject dialsPrefab)
     {
         //Programtically sort default layouts, so if there is an update, i don't need to create a prefab layout
 
