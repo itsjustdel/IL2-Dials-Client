c65c4d31c4715231875c217f32c401d0d4873dcc
Del Reid <45520351+itsjustdel@users.noreply.github.com>
2023-01-26 15:47:01 +0000

    Tidy

--- Assets/Scripts/SlaveManager.cs
+++ Assets/Scripts/SlaveManager.cs
@@ -16,8 +16,6 @@ public class SlaveManager : MonoBehaviour
     public bool slave = false;
     public int id;
     public int monintorNumber = -1;
-    
-
 
     [DllImport("user32.dll", SetLastError = true)]
     internal static extern bool MoveWindow(IntPtr hWnd, int X, int Y, int nWidth, int nHeight, bool bRepaint);
@@ -33,13 +31,7 @@ public class SlaveManager : MonoBehaviour
 #if UNITY_ANDROID
         return;
 #endif
-
         string[] args = Environment.GetCommandLineArgs();
-
-   //     UnityEngine.Debug.Log("args = ");
-     //   foreach(string s in args)
-       //     UnityEngine.Debug.Log(s);
-
 
         //get args and save them to public variables for easy access
         //first arg is file location of exe
@@ -47,21 +39,15 @@ public class SlaveManager : MonoBehaviour
         {
             if (args[1] == "Slave")
             {
-             //   UnityEngine.Debug.Log("This is a slave");
-
                 slave = true;
             }
-
-
             id = int.Parse(args[2]);
-   //         UnityEngine.Debug.Log("id = " + id);
-
         }
         else
         {
-           // UnityEngine.Debug.Log("This is the master");
+            // UnityEngine.Debug.Log("This is the master");
             //the master spawns all slave windows on startup
-            string[] keys = PlayerPrefsHelper.GetRegistryValues();            
+            string[] keys = PlayerPrefsHelper.GetRegistryValues();
             foreach (string key in keys)
             {
                 //layout keys are saved with id then plane type e.g (0 il2 mod 1942), (1 spitfire-123)
@@ -85,20 +71,18 @@ public class SlaveManager : MonoBehaviour
                     int _id = int.Parse(idString);
 
                     //0 is the master
-                    if(_id > 0)
-                        //use this if to spawn a previous slave
+                    if (_id > 0)
+                        //use this if to spawn a previously saved slave
                         SpawnOldSlave(_id);
                 }
             }
         }
     }
 
-    //new slave
     public void SpawnNewSlave()
     {
-
         //create id and pass as arg
-        //find the highest previous id - all salves have a windowInfo key attached find them
+        //find the highest previous id - all slaves have a windowInfo key attached, find them
         //get all player prefs that start with this plane type
         string[] keys = PlayerPrefsHelper.GetRegistryValues();
         int highest = 0;
@@ -108,9 +92,8 @@ public class SlaveManager : MonoBehaviour
             string[] subs = key.Split(' ');
 
             //we are looking for a key to find the highest id
-            if  (subs[0] == "windowInfo")
+            if (subs[0] == "windowInfo")
             {
-
                 //strip id number from registry key which has _hxxxxxxx after it
                 string idString = "";
                 int start = subs[0].Length + 1;
@@ -126,67 +109,53 @@ public class SlaveManager : MonoBehaviour
                 int _id = int.Parse(idString);
                 if (_id > highest)
                     highest = _id;
-
             }
         }
 
         highest += 1;
         string args = "Slave " + highest.ToString();
-
         var process = Process.GetCurrentProcess();
         string fullPath = process.MainModule.FileName;
 
         var myProcess = new Process();
-
         myProcess.StartInfo.FileName = fullPath;
-        myProcess.StartInfo.Arguments = args;     
+        myProcess.StartInfo.Arguments = args;
         myProcess.Start();
     }
 
     public static void SpawnOldSlave(int id)
     {
         string args = "Slave " + id;
-
         var process = Process.GetCurrentProcess();
         string fullPath = process.MainModule.FileName;
-
         var myProcess = new Process();
-
-     
-        //window size and position -use when loading - 
-        //SetWindowPos(myProcess.MainWindowHandle, layout.rect.Top, layout.rect.Left);
 
         myProcess.StartInfo.FileName = fullPath;
         myProcess.StartInfo.Arguments = args;
         myProcess.Start();
-                
     }
 
     public void AddSlaveWindow()
     {
-       SpawnNewSlave();
+        SpawnNewSlave();
     }
 
     public void DeleteScreen()
     {
-        
         //get all player prefs that start with this plane type
         string[] keys = PlayerPrefsHelper.GetRegistryValues();
-
-        //UnityEngine.Debug.Log(keys);        
-
         foreach (string key in keys)
-        {            
+        {
             //layout keys are saved with id then plane type e.g (0 il2 mod 1942), (1 spitfire-123)
             string[] subs = key.Split(' ');
-            
+
             //we are looking for a layout etc
             if (subs[0] == "layout" || subs[0] == "fullscreen" || subs[0] == "windowInfo")
             {
                 //strip id number from registry key which has _hxxxxxxx after it
                 string idString = "";
                 int start = subs[0].Length + 1;
-                
+
                 for (int i = start; i < key.Length - 1; i++)
                 {
                     //look for handle "_h" - we don't need values after key[x] is a char so do conversion                        
@@ -195,23 +164,21 @@ public class SlaveManager : MonoBehaviour
 
                     idString += key[i];
                 }
-                
+
                 string[] subsIdString = idString.Split(' ');
                 int parsed = int.Parse(subsIdString[0]);
                 if (parsed == id)
-                {                
+                {
                     string keyToDelete = subs[0] + " " + idString;
                     UnityEngine.Debug.Log("deleting key = " + keyToDelete);
                     PlayerPrefs.DeleteKey(keyToDelete);
                 }
-                
             }
         }
 
         //and close the app
         UnityEngine.Debug.Log("should close now");
         Application.Quit();
-
     }
 
     public void DeleteConfirm()
@@ -220,18 +187,14 @@ public class SlaveManager : MonoBehaviour
             confirmObject.SetActive(true);
         else
             masterClientText.SetActive(true);
-            
     }
-
 
     public void ScreensPanelBack()
     {
         displayPanel.SetActive(false);
         menuPanel.SetActive(true);
-
         //reset text
         confirmObject.SetActive(false);
         masterClientText.SetActive(false);
     }
-
 }
